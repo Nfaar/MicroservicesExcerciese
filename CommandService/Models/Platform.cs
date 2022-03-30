e679e4351e81a7b6818a1fdb0dfabaed2be07101
Viktor Naydenov <nfaarfaow@gmail.com>
2022-03-30 17:54:30 +0200

    update

---
+++ CommandService/Models/Platform.cs
@@ -0,0 +1,11 @@
+namespace CommandService.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ExternalId { get; set; }
+    }
+}
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,17 +19,30 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            this.env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            if(this.env.IsProduction())
+            {
+                Console.WriteLine("--> Using SQL Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                    opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMem Db");
+                services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            }
 
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -67,8 +80,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
-
-            PrepDb.PrepPopulation(app);
+            
+            PrepDb.PrepPopulation(app, this.env.IsProduction());
         }
     }
 }
